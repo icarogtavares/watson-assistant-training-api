fd157e543129226d33b62e34bc470292c52af603
Icaro Tavares <icarotavares@live.com>
2018-06-13 18:27:05 -0300

    Bug fix

--- src/bin/middlewares.js
+++ src/bin/middlewares.js
@@ -30,10 +30,9 @@ const configureExpress = () => {
     next(err)
   })
 
-  app.use((err, req, res) => {
+  app.use((err, req, res, next) => {
     res.locals.message = err.message
     res.locals.error = req.app.get('env') === 'development' ? err : {}
-
     res.status(err.status || 500)
     res.send(res.locals.error)
   })
