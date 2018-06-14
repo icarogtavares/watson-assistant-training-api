a99abffd22477799e8ecce4049decb18cba59236
Icaro Tavares <icarotav4@gmail.com>
2018-06-13 22:31:02 -0300

    Added watson-assistant environmnent vars validation

--- src/config/watson.assistant.js
+++ src/config/watson.assistant.js
@@ -1,20 +1,33 @@
 const { getConfig } = require('./index')
+const Joi = require('joi')
+
+const schema = Joi.object().keys({
+  username: Joi.string().disallow(['<assistant_username>']),
+  password: Joi.string().disallow(['<assistant_password>']),
+  workspace_id: Joi.string.disallow(['<workspace_id>']),
+}).unknown().required()
+
+const { error, value: envVars } = Joi.validate(process.env, schema)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
 
 const config = getConfig({
   development: {
-    username: process.env.ASSISTANT_USERNAME || '<assistant_username>',
-    password: process.env.ASSISTANT_PASSWORD || '<assistant_password>',
-    workspace_id: process.env.WORKSPACE_ID || '<workspace_id>',
+    username: envVars.ASSISTANT_USERNAME,
+    password: envVars.ASSISTANT_PASSWORD,
+    workspace_id: envVars.WORKSPACE_ID,
   },
   production: {
-    username: process.env.ASSISTANT_USERNAME || '<assistant_username>',
-    password: process.env.ASSISTANT_PASSWORD || '<assistant_password>',
-    workspace_id: process.env.WORKSPACE_ID || '<workspace_id>',
+    username: envVars.ASSISTANT_USERNAME,
+    password: envVars.ASSISTANT_PASSWORD,
+    workspace_id: envVars.WORKSPACE_ID,
   },
   test: {
-    username: process.env.ASSISTANT_USERNAME || '<assistant_username>',
-    password: process.env.ASSISTANT_PASSWORD || '<assistant_password>',
-    workspace_id: process.env.WORKSPACE_ID || '<workspace_id>',
+    username: envVars.ASSISTANT_USERNAME,
+    password: envVars.ASSISTANT_PASSWORD,
+    workspace_id: envVars.WORKSPACE_ID,
   },
 })
 
--- src/controllers/watson.assistant.js
+++ src/controllers/watson.assistant.js
@@ -6,13 +6,6 @@ const config = getConfig()
 
 const message = (req, res) => {
   const workspace = config.workspace_id
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      output: {
-        text: 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the <a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br> Once a workspace has been defined the intents may be imported from <a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.',
-      },
-    })
-  }
   const payload = {
     workspace_id: workspace,
     context: req.body.context || {},
