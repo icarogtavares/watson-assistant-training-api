fad73405af612f4cdb9c552c2c11b8114f8ff8a8
Icaro Tavares <icarotav4@gmail.com>
2018-06-13 21:25:32 -0300

    Code adjustment

--- src/bin/watson.assistant.js
+++ src/bin/watson.assistant.js
@@ -1,9 +1,11 @@
 const watson = require('watson-developer-cloud')
+const getConfig = require('../config/watson.assistant')
+let config = getConfig();
 
 var assistant = new watson.AssistantV1({
-    username: process.env.ASSISTANT_USERNAME || '<username>',
-    password: process.env.ASSISTANT_PASSWORD || '<password>',
-    version: '2018-02-16'
-  });
+  username: config.username || '<username>',
+  password: config.password || '<password>',
+  version: '2018-06-13'
+});
 
 module.exports = assistant
